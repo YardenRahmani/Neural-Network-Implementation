348bd4e1d5bd1b8245cbce4444a0dcddce3daed5
YardenRahmani <yarde3350@gmail.com>
2023-11-13 19:47:31 +0200

    script added data cleaning after run. started to split in functions the main file

--- test_script.py
+++ test_script.py
@@ -1,4 +1,5 @@
 import subprocess
+import os
 import random
 
 LEARNING_RATE = 0.001
@@ -24,20 +25,32 @@ def generate_data(testNum):
                 dataFile.write(f"{x1} {x2} {3*x1 + 2*x2 + 5}\n")
         return [fileName, str(inputSize), str(outputSize)]
 
+def clear_data(test_num):
+    user_input = input("Delete generated data files? [y/n]")
+    while user_input not in ['y', 'n']:
+        user_input = input("Wrong input. [y/n]")
+    if user_input == 'y':
+        for iter in range(1, test_num + 1):
+            try:
+                os.remove(f"data{iter}")
+            except Exception as e:
+                print(f"Failed to delete data{iter}. error: {e}")
+
+cur_test = 1
 # test 1
 print("test 1")
-args = generate_data(1)
+args = generate_data(cur_test)
 cmdLine = ["python3", "training_neural_network.py"] + args + consts
 process = subprocess.run(cmdLine, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
-if process.returncode != 0:
-    print(f"error in test 1")
-print(process.stdout)
+print(f"error in test 1\n{process.stderr}") if process.returncode != 0 else print(process.stdout)
+    
 
 # test 2
+cur_test += 1
 print("test 2")
-args = generate_data(2)
+args = generate_data(cur_test)
 cmdLine = ["python3", "training_neural_network.py"] + args + consts
 process = subprocess.run(cmdLine, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
-if process.returncode != 0:
-    print(f"error in test 2")
-print(process.stdout)
+print(f"error in test 2\n{process.stderr}") if process.returncode != 0 else print(process.stdout)
+
+clear_data(cur_test)
--- training_neural_network.py
+++ training_neural_network.py
@@ -1,29 +1,31 @@
 import sys
 import numpy as np
 
-def read_data():
-    input = []
-    output = [] 
-    with open(sys.argv[1], "r") as dataFile:
+def read_data(file_name, input_size, output_size):
+    input_data = []
+    output_data = []
+    with open(file_name, "r") as dataFile:
         for line in dataFile.readlines():
             line = list(map(float,line.strip().split()))
-            input.append(line[:inputSize])
-            output.append(line[inputSize:])
-    return np.array(input), np.array(output)
+            if len(line) != input_size + output_size:
+                return None, None, False
+            input_data.append(line[:input_size])
+            output_data.append(line[input_size:])
+    return np.array(input_data), np.array(output_data), True
 
-def train_model():
-    LEARNING_RATE = float(sys.argv[4])
-    HIDDEN_LAYER_SIZE = int(sys.argv[5])
-    SAMPLES = int(sys.argv[6])
-    EPOCHES = int(sys.argv[7])
+def initialize_weights(input_size, output_size, hidden_layer_size):
     np.random.seed(0)
-    W1 = np.random.randn(inputSize,HIDDEN_LAYER_SIZE)
-    b1 = np.random.randn(1,HIDDEN_LAYER_SIZE)
-    W2 = np.random.randn(HIDDEN_LAYER_SIZE,HIDDEN_LAYER_SIZE)
-    b2 = np.random.randn(1, HIDDEN_LAYER_SIZE)
-    W3 = np.random.randn(HIDDEN_LAYER_SIZE,outputSize)
-    b3 = np.random.randn(1, outputSize)
-    for epoch in range(EPOCHES):
+    W1 = np.random.randn(input_size,hidden_layer_size)
+    b1 = np.random.randn(1,hidden_layer_size)
+    W2 = np.random.randn(hidden_layer_size,hidden_layer_size)
+    b2 = np.random.randn(1, hidden_layer_size)
+    W3 = np.random.randn(hidden_layer_size,output_size)
+    b3 = np.random.randn(1, output_size)
+    return W1, W2, W3, b1, b2, b3
+
+def train_model(X, Y, learning_rate,  hidden_layer_size, samples, epochs):
+    W1, W2, W3, b1, b2, b3 = initialize_weights(X.shape[1], Y.shape[1], hidden_layer_size)
+    for epoch in range(epochs):
         H1 = np.dot(X,W1) + b1
         H2 = np.dot(H1,W2) + b2
         Y_pred = np.dot(H2,W3) + b3
@@ -31,25 +33,35 @@ def train_model():
         MSE = np.mean(error**2)
         #print(f"MSE: {MSE}")
         gradMSE2out = 2*error ## dL/dy = 2*error
-        gradMSE2W3 = np.dot(H2.T, gradMSE2out) / SAMPLES ## dL/dw3 = (dL/dy)(dy/dW3) = (dL/dy)H2
+        gradMSE2W3 = np.dot(H2.T, gradMSE2out) / samples ## dL/dw3 = (dL/dy)(dy/dW3) = (dL/dy)H2
         gradMSE2b3 = np.mean(gradMSE2out) ## dL/db3 = (dL/dy)(dy/b3) = dL/dy
-        gradMSE2H2 = np.dot(gradMSE2out, W3.T) / SAMPLES ## dL/dH2 = (dL/dy)(dy/dH2) = (dL/dy)W3
-        gradMSE2W2 = np.dot(H1.T, gradMSE2H2) / SAMPLES## dL/dW2 = (dL/dH2)(dH2/dW2) = (dL/dH2)H1
+        gradMSE2H2 = np.dot(gradMSE2out, W3.T) / samples ## dL/dH2 = (dL/dy)(dy/dH2) = (dL/dy)W3
+        gradMSE2W2 = np.dot(H1.T, gradMSE2H2) / samples## dL/dW2 = (dL/dH2)(dH2/dW2) = (dL/dH2)H1
         gradMSE2b2 = np.mean(gradMSE2H2) ## dL/db2 = (dL/dH2)(dH2/db2) = dL/dH2
-        gradMSE2H1 = np.dot(gradMSE2H2, W2.T) / SAMPLES ## dL/dH1 = (dL/dH2)(dH2/dH1) = (dL/dH2)W2
-        gradMSE2W1 = np.dot(X.T, gradMSE2H1) / SAMPLES ## dL/dW1 = (dL/dH1)(dH1/dW1) = (dL/dH)X
+        gradMSE2H1 = np.dot(gradMSE2H2, W2.T) / samples ## dL/dH1 = (dL/dH2)(dH2/dH1) = (dL/dH2)W2
+        gradMSE2W1 = np.dot(X.T, gradMSE2H1) / samples ## dL/dW1 = (dL/dH1)(dH1/dW1) = (dL/dH)X
         gradMSE2b1 = np.mean(gradMSE2H1) ## dL/db1 = (dL/dH)(dH/db1) = dL/dH
-        W3 -= LEARNING_RATE * gradMSE2W3
-        b3 -= LEARNING_RATE * gradMSE2b3
-        W2 -= LEARNING_RATE * gradMSE2W2
-        b2 -= LEARNING_RATE * gradMSE2b2
-        W1 -= LEARNING_RATE * gradMSE2W1
-        b1 -= LEARNING_RATE * gradMSE2b1
+        W3 -= learning_rate * gradMSE2W3
+        b3 -= learning_rate * gradMSE2b3
+        W2 -= learning_rate * gradMSE2W2
+        b2 -= learning_rate * gradMSE2b2
+        W1 -= learning_rate * gradMSE2W1
+        b1 -= learning_rate * gradMSE2b1
         if MSE < 1e-05:
             print(f"after {epoch + 1} iterations: MSE = {MSE}")
             break
 
-inputSize = int(sys.argv[2])
-outputSize = int(sys.argv[3])
-X, Y = read_data()
-train_model()
+if __name__ == "__main__":
+    file_name = sys.argv[1]
+    input_size = int(sys.argv[2])
+    output_size = int(sys.argv[3])
+    X, Y, valid = read_data(file_name, input_size, output_size)
+    if valid == False:
+        print("Bad data file")
+    else:
+        learning_rate = float(sys.argv[4])
+        hidden_layer_size = int(sys.argv[5])
+        samples = int(sys.argv[6])
+        epochs = int(sys.argv[7])
+        print(X.shape[1])
+        train_model(X, Y, learning_rate, hidden_layer_size, samples, epochs)
